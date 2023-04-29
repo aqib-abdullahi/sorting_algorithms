b9356cc152896df4dd1745f7c19dd53edae100c2
aqib-abdullahi <abdullahiaqib@gmail.com>
2023-04-29 06:28:26 -0700

    insertion sort algorithm implementation TASK 1

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include "sort.h"
+
+/**
+ * insertion_sort_list - sorts a doubly linked list
+ * with the insertion sort algorithm
+ * @list: List to sort
+ * Return: Nothing
+ */
+
+void insertion_sort_list(listint_t **list)
+{
+	bool flag = false;
+	listint_t *tmp = NULL;
+	listint_t *aux = NULL;
+
+	if (!list || !(*list) || !(*list)->next)
+		return;
+
+	tmp = *list;
+	while (tmp->next)
+	{
+		if (tmp ->n > tmp->next->n)
+		{
+			tmp->next->prev = tmp->prev;
+			if (tmp->next->prev)
+			{
+				tmp->prev->next = tmp->next;
+			}
+			else
+				*list = tmp->next;
+
+			tmp->prev = tmp->next;
+			tmp->next = tmp->next->next;
+			tmp->prev->next = tmp;
+			if (tmp->prev && tmp->prev->n > tmp->n)
+			{
+				tmp->next->prev = tmp;
+			}
+
+			tmp = tmp->prev;
+			print_list(*list);
+
+			if (tmp->prev && tmp->prev->n > tmp->n)
+			{
+				if (!flag)
+					aux = tmp->next;
+				flag = true;
+				tmp = tmp->prev;
+				continue;
+			}
+		}
+		if (!flag)
+			tmp = tmp->next;
+		else
+		{
+			tmp = aux;
+			flag = false;
+		}
+	}
+}
